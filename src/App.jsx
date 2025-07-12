022ddadf18022bd0d3b7fb5a60e8f2e7158ed0bb
Krisha Kapadia <krisha.kapadia@somaiya.edu>
2025-07-13 01:21:11 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+    <h2>Hello welcome to my project</h2>
+    <Button>Subcribe</Button>
+    </>
+  )
+}
+
+export default App
